e1b9406ec4dc1d62457973b9d40e71819f7b891f
Guy Geva <guy1030@gmail.com>
2021-08-09 15:41:42 +0300

    Some chart added + sparkline library

--- src/cmps/Charts.jsx
+++ src/cmps/Charts.jsx
@@ -1,8 +1,33 @@
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
-export function Charts() {
-  return (
-    <div>
-      Charts
-    </div>
-  )
+import { Component } from 'react'
+import { bitcoinService } from "../services/bitcoinService";
+
+export class Charts extends Component {
+  state = {
+    marketPrice: null
+  }
+  componentDidMount() {
+    this.loadInfo()
+  }
+
+  loadInfo = async () => {
+    console.log('loadInfo');
+    const marketPrice = await bitcoinService.getMarketPrice()
+    this.setState({ marketPrice: marketPrice.values })
+    console.log(this.state);
+  }
+  render() {
+    if (!this.state.marketPrice) return <div>Loading..</div>
+    const { marketPrice } = this.state
+    return (
+      <div>
+        <h2 style={{'textAlign': 'center'}}>Market price</h2>
+        <Sparklines data={marketPrice.map(val => [val.y.toFixed()])} >
+          <SparklinesLine color="silver" />
+        </Sparklines>
+      </div>
+    )
+  }
 }
+
